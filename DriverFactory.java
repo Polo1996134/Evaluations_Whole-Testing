1c3114cfe6cf9928d3bf3f1ccd3de9634328d136
Poulomi Roy <proy14268@gmail.com>
2025-05-27 13:00:31 +0530

    DriverFactory.java

--- DriverFactory.java
+++ DriverFactory.java
@@ -1,5 +1,30 @@
+
 package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized"); 
+            options.addArguments("--disable-notifications");
+            driver = new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
