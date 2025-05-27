cb36d710618787e67d39e8524e1164ae98d5fa8f
Poulomi Roy <proy14268@gmail.com>
2025-05-27 13:02:32 +0530

    BaseTest.java

--- BaseTest.java
+++ BaseTest.java
@@ -1,5 +1,22 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
 public class BaseTest {
 
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.getDriver();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
